22d376586baeb5a451f547108e65c9e2dcbdbccc
TaiNT24 <nguyentutai12@gmail.com>
2024-11-02 22:11:18 +0700

    test5

--- nodejs-slave/start.sh
+++ nodejs-slave/start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo "-jar /usr/share/jenkins/agent.jar -url $JENKINS_URL -secret $SECRET -name $SLAVE_NAME -webSocket"
+
+# service docker start
+# timeout 30s bash -c 'until sudo docker info >/dev/null 2>&1; do echo "Waiting for Docker to start..."; sleep 1; done'
+# exec jenkins-agent "$@"
 java -jar /usr/share/jenkins/agent.jar -url "$JENKINS_URL" -secret "$SECRET" -name "$SLAVE_NAME" -webSocket
