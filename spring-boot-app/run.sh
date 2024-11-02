8c2990ba6d7dfeafb3040fffc83c1605a87638c3
TaiNT24 <nguyentutai12@gmail.com>
2024-11-03 00:58:56 +0700

    test20

--- spring-boot-app/run.sh
+++ spring-boot-app/run.sh
@@ -4,11 +4,3 @@
 nohup java -jar target/${JAR_NAME} --server.port=${APP_PORT} > app.log 2>&1 &  
 
 sleep 40  # Give some time for the application to start  
-
-# Check if process is running  
-if ps -p $(cat $PID_FILE) > /dev/null; then  
-    echo "Application started successfully"  
-else  
-    echo "Failed to start application"  
-    exit 1  
-fi
