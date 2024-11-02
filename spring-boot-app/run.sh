7c6ed3ce14ac8d9ba928f24f7f6c21b91d228af8
TaiNT24 <nguyentutai12@gmail.com>
2024-11-03 01:03:11 +0700

    test20

--- spring-boot-app/run.sh
+++ spring-boot-app/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 
 # Start the application  
-nohup java -jar target/${JAR_NAME} --server.port=${APP_PORT} > app.log 2>&1 &  
+nohup java -jar target/hello-world-spring-1.0.0.jar --server.port=3001 > app.log 2>&1 &  
 
 sleep 40  # Give some time for the application to start  
