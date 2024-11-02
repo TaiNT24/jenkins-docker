d8928f181c5985759236242d4a8703a72a1f895a
TaiNT24 <nguyentutai12@gmail.com>
2024-11-03 00:57:17 +0700

    test18

--- spring-boot-app/run.sh
+++ spring-boot-app/run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash  
-PID_FILE=application.pid  
-
-# Kill existing process if running  
-if [ -f $PID_FILE ]; then  
-    kill -9 $(cat $PID_FILE) || true  
-    rm $PID_FILE  
-fi  
 
 # Start the application  
 nohup java -jar target/${JAR_NAME} --server.port=${APP_PORT} > app.log 2>&1 &  
-echo $! > $PID_FILE  
-sleep 10  # Give some time for the application to start  
+
+sleep 40  # Give some time for the application to start  
 
 # Check if process is running  
 if ps -p $(cat $PID_FILE) > /dev/null; then  
